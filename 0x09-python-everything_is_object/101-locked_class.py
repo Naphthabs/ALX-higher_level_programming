ec97b29ff4f95e00bf590aee578e57b8585ad948
Naphthabs <tnthekiso@yahoo.com>
2024-01-18 12:41:37 -0800

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""This defines a locked class"""
+"""Defines a locked class."""
 
 
 class LockedClass:
+    """
+    Preventing the user from instantiating our LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
 
     __slots__ = ["first_name"]
